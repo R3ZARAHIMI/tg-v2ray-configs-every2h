8fdf34c0a448e8aa49e6e896ba15f4d0cbb190e9
R3ZARAHIMI <r3z4rahimy@gmail.com>
2025-07-05 03:30:57 +0330

    Bot-del

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # تنظیمات
 CHANNELS = [
